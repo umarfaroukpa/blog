75114349e4fd803735e6ca16f36eb1cccce361c1
Umar Farouk <yasmarfaq@yahoo.com>
2025-04-22 14:19:21 +0100

    correcting build

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/homepage.tsx';
+import HomePage from './pages/HomePage.tsx';
 import PostPage from './pages/PostPage';
 import TagPage from './pages/tagpage.tsx';
 import AboutPage from './pages/aboutpage.tsx'
